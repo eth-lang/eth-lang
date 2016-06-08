91123ee63ebfa8498cb046b9decde7f846d0cb5f
Frederic Gingras <frederic@gingras.cc>
2016-06-08 04:07:59 -0400

    parser: improve comment in empty expressions and last expression in ast

--- parser.go
+++ parser.go
@@ -41,33 +41,63 @@ func parseData(ast *AST, data string, fileIndex int) (err error) {
 
 	lex := lex(ast.Files[fileIndex], data)
 
+	appendItem := func(n *Node, i item) {
+		n.Children = append(n.Children, &Node{
+			File: fileIndex,
+			Line: i.line,
+			Col:  i.col,
+			Data: i.val,
+			Type: i.typ,
+		})
+	}
+
+	appendExpression := func(n *Node) {
+		n.Children = append(n.Children, &Node{
+			Type:   itemExpression,
+			File:   fileIndex,
+			Parent: n,
+		})
+	}
+
+	deleteLastChildren := func(n *Node) {
+		n.Children = n.Children[:len(n.Children)-1]
+	}
+
 	i := lex.nextItem()
 	for i.typ != itemEOF {
 		switch i.typ {
 		case itemError:
 			return NewParseError(ast.Files[fileIndex], i.line, i.col, i.val)
 		case itemNewLine:
-			if node.Type == itemExpression && len(node.Children) > 0 {
+			if len(node.Children) > 0 {
 				// TODO check expression is terminated
 				// start a new expression and set that as our current node
-				node.Parent.Children = append(node.Parent.Children, &Node{
-					Type:   itemExpression,
-					File:   fileIndex,
-					Parent: node.Parent,
-				})
+				appendExpression(node.Parent)
+				fmt.Println(ast)
 				node = node.Parent.Children[len(node.Parent.Children)-1]
 			}
+		case itemComment:
+			if node.Type == itemExpression && len(node.Children) == 0 {
+				// remove empty expression, add comment, add empty expression
+				deleteLastChildren(node.Parent)
+				appendItem(node.Parent, i)
+				appendExpression(node.Parent)
+
+				// set current node to that new empty expression
+				node = node.Parent.Children[len(node.Parent.Children)-1]
+			} else {
+				appendItem(node, i)
+			}
 		default:
-			node.Children = append(node.Children, &Node{
-				File: fileIndex,
-				Line: i.line,
-				Col:  i.col,
-				Data: i.val,
-				Type: i.typ,
-			})
+			appendItem(node, i)
 		}
 
 		i = lex.nextItem()
+	}
+
+	// if we are left with an empty expression at the end
+	if node.Type == itemExpression && len(node.Children) == 0 {
+		node.Parent.Children = node.Parent.Children[:len(node.Parent.Children)-1]
 	}
 
 	return nil
